ff5c5fe8e4322058dde113dd0a236a06ffe0d0fe
Janvi Kamble <janvikamble50@gmail.com>
2026-07-21 23:03:00 +0530

    Add files via upload

---
+++ LinkedList/DoublyLinkedList/backtravel.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Backward traversal of doubly Linked List
+
+struct Node
+{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+void display(struct Node* head){
+    struct Node* ptr = head;
+   
+    //first take ptr to the last node
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    //then tranverse backward
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr-> prev;
+    }
+    
+    
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    struct Node*head;
+    struct Node*second;
+    struct Node*third;
+    struct Node*fourth;
+
+  head = (struct Node*)malloc(sizeof(struct Node));
+  second = (struct Node*)malloc(sizeof(struct Node));
+  third = (struct Node*)malloc(sizeof(struct Node));
+  fourth = (struct Node*)malloc(sizeof(struct Node));
+
+  head->data = 10;
+    head->prev = NULL;
+    head->next = second;
+
+    second->data = 20;
+    second->prev = head;
+    second->next = third;
+
+    third->data = 30;
+    third->prev = second;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->prev = third;
+    fourth->next = NULL;
+
+  display(head);
+
+    return 0;
+}
+
