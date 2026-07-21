ae67fa3916e4729a7eaa7ce8e7c6334c989e966d
Janvi Kamble <janvikamble50@gmail.com>
2026-07-21 23:03:49 +0530

    Add files via upload

---
+++ LinkedList/DoublyLinkedList/deleteatbegin.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+//delete the node at the begining of a doubly linked list
+
+struct Node
+{
+    int data;
+    struct Node*prev;
+    struct Node*next;
+};
+
+void display(struct Node*head){
+    struct Node* ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ",ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node* deleteatfirst(struct Node*head){
+    struct Node*ptr = head;
+    head = head->next;
+    if (head !=NULL)
+    {
+        head-> prev = NULL;
+    }
+    free(ptr);
+    return head;
+    
+}                                          
+
+int main(int argc, char const *argv[])
+{
+     struct Node*head;
+    struct Node*second;
+    struct Node*third;
+    struct Node*fourth;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->prev = NULL;
+    head->next = second;
+
+    second->data = 20;
+    second->prev = head;
+    second->next = third;
+
+    third->data = 30;
+    third->prev = second;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->prev = third;
+    fourth->next = NULL;
+
+    printf("\nbefore: ");
+    display(head);
+    head = deleteatfirst(head);
+    printf("\nafter: ");
+    display(head);
+
+    return 0;
+}
+
