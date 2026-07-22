907d3a5e09faf02d55d65aae1227fa282b8183bd
Janvi Kamble <janvikamble50@gmail.com>
2026-07-22 22:53:35 +0530

    Add files via upload

---
+++ LinkedList/DoublyLinkedList/reverse.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+//reverse doubly Linked list
+
+struct Node
+{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+void display(struct Node* head){
+    struct Node* ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node* reverse(struct Node* head)
+{
+    struct Node* current = head;
+    struct Node* temp = NULL;
+
+    while (current != NULL)
+    {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+
+        current = current->prev;
+    }
+
+    if (temp != NULL)
+    {
+        head = temp->prev;
+    }
+
+    return head;
+}
+
+int main(int argc, char const *argv[])
+{
+    struct Node*head;
+    struct Node*second;
+    struct Node*third;
+    struct Node*fourth;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->prev = NULL;
+    head->next = second;
+
+    second->data = 20;
+    second->prev = head;
+    second->next = third;
+
+    third->data = 30;
+    third->prev = second;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->prev = third;
+    fourth->next = NULL;
+    return 0;
+}
+
